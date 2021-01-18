e5fe62ef0d5cb7c7df4a467a0d1ccd8a62ab194b
廉竣为 <534897141@qq.com>
2021-01-18 13:01:18 +0800

    Scanner 类

---
+++ example-java/src/com/api5/ex1Scanner.java
@@ -0,0 +1,25 @@
+package com.api5;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.Scanner;
+
+public class ex1Scanner {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num=in.nextInt();
+       System.out.println(num);
+
+        int a,b,c;
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        int mx= Math.max(a,Math.max(b,c));
+        System.out.println(mx);
+
+        /*
+        *使用匿名对象
+         */
+        System.out.println(new Scanner(System.in).nextInt());
+
+    }
+}
