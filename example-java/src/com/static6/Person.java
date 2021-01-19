763417bf6fd4c970e9e483da03030a3e9a24bfd5
廉竣为 <534897141@qq.com>
2021-01-20 00:27:15 +0800

    static 关键字

---
+++ example-java/src/com/static6/Person.java
@@ -0,0 +1,14 @@
+package com.static6;
+
+
+public class Person {
+
+    static {
+        System.out.println("静态代码块执行！");
+    }
+
+    public Person() {
+        System.out.println("构造方法执行！");
+    }
+
+}
