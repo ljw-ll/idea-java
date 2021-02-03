d0e00b86ad7cc5b854b82ded8692c5c29995d1be
廉竣为 <534897141@qq.com>
2021-02-03 18:01:06 +0800

    线程的概念

---
+++ example-java/src/comAdvance/day05/ex1Thread/MyThread.java
@@ -0,0 +1,11 @@
+package comAdvance.day05.ex1Thread;
+
+public class MyThread  extends  Thread{
+
+    @Override
+    public void run() {
+        for (int i = 0; i <20 ; i++) {
+            System.out.println("run:"+i);
+        }
+    }
+}
