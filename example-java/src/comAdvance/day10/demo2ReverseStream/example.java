7da762934034911013fe8b567a67735a7fa94a96
廉竣为 <534897141@qq.com>
2021-02-18 20:06:36 +0800

    转换流

---
+++ example-java/src/comAdvance/day10/demo2ReverseStream/example.java
@@ -0,0 +1,35 @@
+package comAdvance.day10.demo2ReverseStream;
+
+
+import java.io.*;
+
+/*
+    练习：转换文件编码
+        将GBK编码的文本文件，转换为UTF-8编码的文本文件。
+
+    分析:
+        1.创建InputStreamReader对象,构造方法中传递字节输入流和指定的编码表名称GBK
+        2.创建OutputStreamWriter对象,构造方法中传递字节输出流和指定的编码表名称UTF-8
+        3.使用InputStreamReader对象中的方法read读取文件
+        4.使用OutputStreamWriter对象中的方法write,把读取的数据写入到文件中
+        5.释放资源
+ */
+public class example {
+    public static void main(String[] args) {
+
+        try(
+        InputStreamReader isr=new InputStreamReader(new FileInputStream("example-java\\src\\comAdvance\\day10\\gbk.txt"),"GBK");
+        OutputStreamWriter osw=new OutputStreamWriter(new FileOutputStream("example-java\\src\\comAdvance\\day10\\gbk_utf.txt"),"utf-8");
+        ){
+            char[] cs=new char[1024];
+            int len=0;
+            while((len=isr.read(cs))!=-1){
+                osw.write(cs,0,len);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
